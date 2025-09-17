785d7186c77f226e8a3355a8d05e36e4109038c8
BadTrL <sacha.cassereau@gmail.com>
2025-09-17 17:12:46 +0200

    Revert "feature: add chunker/parser"
    This reverts commit 80f8d78924cffa07878354d220f2038c603ed378.

--- textnormx/__init__.py
+++ textnormx/__init__.py
@@ -1,17 +1,2 @@
-from .cleaner.normalize import clean_text, clean_chunks 
-
-# API parser 
-from .parser import (
-    Element, Chunk,
-    parse_markdown_elements, parse_html_elements,
-    chunk_by_title,
-)
-
-__all__ = [
-    # cleaner
-    "clean_text", "clean_chunks",
-    # parser
-    "Element", "Chunk",
-    "parse_markdown_elements", "parse_html_elements",
-    "chunk_by_title",
-]
+from .normalize import clean_text, clean_lines, clean_chunks
+__all__ = ["clean_text", "clean_lines", "clean_chunks"]
